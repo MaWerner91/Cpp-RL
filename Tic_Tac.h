dd873b0c5fd01ae7bd4c2ddaf3fc475c8afb4cb5
Matthias Werner <matthias-werner@fn.de>
2017-07-08 20:46:25 +0200

    Added parameter names to header

--- Tic_Tac.h
+++ Tic_Tac.h
@@ -13,9 +13,9 @@ class TicTacState : public StateVector
         
     public:
     
-        TicTacState(int , int);
+        TicTacState(int dims, int st_per_dim);
         bool *get_possible_actions();
-        void apply_action(int, int);
+        void apply_action(int action, int token);
         void print_state();
         int reward();
 };
